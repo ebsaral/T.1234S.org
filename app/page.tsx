8c58e14b94cace2e286b709779ecd1d858e2853c
Emin Saral <eminbugrasaral@me.com>
2026-05-15 21:05:06 +0300

    Add link to Trabzonspor's website

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,17 @@ export default function Home() {
   return (
     <main className="w-auto min-h-screen flex items-center justify-center bg-black">
       <div className="w-60 animate-breathe">
-        <Image
-          src="/trabzonspor-blue.png"
-          alt="Trabzonspor logo"
-          title="Trabzonspor"
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="w-full h-auto"
-          loading="eager"
-        />
+        <a href="https://www.trabzonspor.org.tr" title="Trabzonspor">
+          <Image
+            src="/trabzonspor-blue.png"
+            alt="Trabzonspor logo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+            loading="eager"
+          />
+        </a>
       </div>
     </main>
   );
